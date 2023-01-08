cdd3814992930deca7f704cf52ccc9a7d32c96a5
Rafael Acosta <theGitNoob@gmail.com>
2023-01-07 22:39:18 -0500

    Added item equip to hero and item removal from hero

--- Logic/Cards/Card.cs
+++ Logic/Cards/Card.cs
@@ -30,17 +30,65 @@ public class SimpleCard : ICard
 }
 public class ItemCard : SimpleCard
 {
-
+    //Holds the Hero to which the item is equiped
+    private HeroCard? Hero;
     public ItemCard(string name, string description, string effect) : base(name, description, effect)
     {
         Type = CardType.Item;
+        Hero = null;
 
     }
+
+    //Add the equipped hero with this item
+    public void EquipToHero(HeroCard hero)
+    {
+        if (Hero != null)
+        {
+            throw new Exception("Cannot equip the same item to two or more heroes");
+        }
+
+        Hero = hero;
+    }
+
+
+    //Removes the item
+    public void RemoveFromHero()
+    {
+        if (Hero == null)
+        {
+            throw new Exception("Can't remove the item from a hero if not equiped");
+        }
+
+        Hero = null;
+
+    }
+
+
+
 }
 public class HeroCard : SimpleCard
 {
+    //Holds the equiped items of the hero
+    private List<ItemCard> _items;
     public HeroCard(string name, string description, string effect) : base(name, description, effect)
     {
         Type = CardType.Hero;
+
+        _items = new List<ItemCard>();
+    }
+
+
+    //Equips the given item to the hero
+    public void EquipItem(ItemCard item)
+    {
+        _items.Add(item);
+
+    }
+
+    //Removes the item from the equiped items
+    public void RemoveItem(ItemCard item)
+    {
+        _items.Remove(item);
+
     }
 }
