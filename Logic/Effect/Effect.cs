a2b5393a9234321f762004298c681a1a9d43c3b2
Rafael Acosta <theGitNoob@gmail.com>
2023-02-02 11:46:58 -0500

    Changed Effect Logic

--- Logic/Effect/Effect.cs
+++ Logic/Effect/Effect.cs
@@ -1,207 +1,198 @@
 ﻿using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using System.Text.Json.Serialization;
+namespace Habilitie;
 
-namespace Effect
+///<summary>
+///Represents an effect
+///</summary>
+public class Effect
 {
     ///<summary>
-    ///Represents an effect
+    ///The condition of the effect as a string
     ///</summary>
-    public class Habilitie<T>
+    public string ConditionString { get; protected init; }
+
+    ///<summary>
+    ///The action for the effect as a string
+    ///</summary>
+    public string ActionString { get; protected init; }
+
+    ///<summary>
+    ///The imports for the condition and action used by `CSharpScript`
+    ///</summary>
+    public string[] Imports { get; protected set; }
+
+
+    ///<summary>
+    ///Creates a new Effect
+    ///</summary>
+    ///<param name="conditionString">The condition for the effect</param>
+    ///<param name="actionString">The action for the effect</param>
+    ///<exception cref="ArgumentNullException">Thrown when the condition or action is null</exception>
+    ///<returns>A new effect</returns>
+    [JsonConstructor]
+    public Effect(string conditionString, string actionString, params string[] imports)
     {
-        ///<summary>
-        ///The condition for the effect
-        ///</summary>
-        [JsonIgnore]
-        public Condition<T> _condition { get; private init; }
+        if (conditionString == null) throw new ArgumentNullException(nameof(conditionString));
+        if (actionString == null) throw new ArgumentNullException(nameof(actionString));
 
-        ///<summary>
-        ///The action for the effect
-        ///</summary>
-        [JsonIgnore]
-        public Action<T> _action { get; private init; }
+        this.ConditionString = conditionString;
+        this.ActionString = actionString;
 
-        ///<summary>
-        ///The condition for the effect as a string
-        ///</summary>
-        public string ConditionString { get => _condition.ConditionString; }
-
-        ///<summary>
-        ///The action for the effect as a string
-        ///</summary>
-        public string ActionString { get => _action.ActionString; }
-
-        ///<summary>
-        ///Creates a new effect
-        ///</summary>
-        ///<param name="condition">The condition for the effect</param>
-        ///<param name="action">The action for the effect</param>
-        ///<exception cref="ArgumentNullException">Thrown when the condition or action is null</exception>
-        ///<returns>A new effect</returns>
-        public Habilitie(Condition<T> condition, Action<T> action)
-        {
-            if (condition == null) throw new ArgumentNullException(nameof(condition));
-            if (action == null) throw new ArgumentNullException(nameof(action));
-
-            this._condition = condition;
-            this._action = action;
-        }
-
-        ///<summary>
-        ///Creates a new effect
-        ///</summary>
-        ///<param name="conditionString">The condition for the effect</param>
-        ///<param name="actionString">The action for the effect</param>
-        ///<example>
-        ///new Habilitie("p1.Health > p2.Health", "p1.Health -= 10")
-        ///</example>
-        ///<exception cref="ArgumentNullException">Thrown when the condition or action is null</exception>
-        ///<returns>A new effect</returns>
-        [JsonConstructor]
-        public Habilitie(string conditionString, string actionString)
-        {
-            if (conditionString == null) throw new ArgumentNullException(nameof(conditionString));
-            if (actionString == null) throw new ArgumentNullException(nameof(actionString));
-
-            this._condition = new Condition<T>(conditionString);
-            this._action = new Action<T>(actionString);
-        }
-
-        ///<summary>
-        ///Checks if the effect can be activated
-        ///</summary>
-        ///<param name="p1">The first player</param>
-        ///<param name="p2">The second player</param>
-        ///<exception cref="ArgumentNullException">Thrown when the first or second player is null</exception>
-        ///<returns>True if the effect can be activated, false otherwise</returns>
-        public bool CanActivate(T p1, T p2)
-        {
-            if (p1 == null || p2 == null) throw new ArgumentNullException();
-            return _condition.Evaluate(p1, p2);
-        }
-
-        ///<summary>
-        ///Activates the effect
-        ///</summary>
-        ///<param name="p1">The first player</param>
-        ///<param name="p2">The second player</param>
-        ///<exception cref="ArgumentNullException">Thrown when the first or second player is null</exception>
-        public void Activate(T p1, T p2)
-        {
-            if (p1 == null || p2 == null) throw new ArgumentNullException();
-            if (CanActivate(p1, p2))
-                _action.Execute(p1, p2);
-        }
-
+        this.Imports = imports;
     }
 
     ///<summary>
-    ///Represents an contion for an `Effect`
+    ///Checks if the condition and action are correct
     ///</summary>
-    public class Condition<T>
+    ///<param name="conditionString">The condition for the effect</param>
+    ///<param name="actionString">The action for the effect</param>
+    ///<param name="imports">The imports for the condition and action used by `CSharpScript`</param>
+    ///<exception cref="ArgumentNullException">Thrown when the condition or action is null</exception>
+    ///<returns>True if the condition and action are correct</returns> 
+    public static bool CheckIsCorrect<T>(string conditionString, string actionString, params string[] imports)
     {
-        ///<summary>
-        ///The condition to evaluate
-        ///</summary>
-        [JsonIgnore]
-        public Func<T, T, bool> _condition { get; init; }
+        if (conditionString == null) throw new ArgumentNullException(nameof(conditionString));
+        if (actionString == null) throw new ArgumentNullException(nameof(actionString));
 
-        ///<summary>
-        ///The condition as a string
-        ///</summary>
-        public string ConditionString { get; private init; }
+        return Condition.CheckIsCorrect<T>(conditionString, imports) && Action.CheckIsCorrect<T>(actionString, imports);
+    }
 
-        ///<summary>
-        ///Evaluates the condition
-        ///</summary>
-        ///<param name="p1">The first player</param>
-        ///<param name="p2">The second player</param>
-        ///<exception cref="ArgumentNullException">Thrown when the first or second player is null</exception>
-        ///<returns>True if the condition is true, false otherwise</returns>
-        public bool Evaluate(T p1, T p2)
-        {
-            if (p1 == null || p2 == null) throw new ArgumentNullException();
-            return _condition.Invoke(p1, p2);
-        }
+    ///<summary>
+    ///Checks if the effect can be activated
+    ///</summary>
+    ///<param name="p1">The first parameter</param>
+    ///<param name="p2">The second parameter</param>
+    ///<exception cref="ArgumentNullException">Thrown when the parameters are null</exception>
+    ///<returns>True if the effect can be activated</returns>
+    public bool CanActivate<T>(T p1, T p2, params string[] imports)
+    {
+        if (p1 == null) throw new ArgumentNullException(nameof(p1));
+        if (p2 == null) throw new ArgumentNullException(nameof(p2));
 
-        ///<summary>
-        ///Creates a new condition
-        ///</summary>
-        ///<param name="conditionString">The condition to evaluate</param>
-        ///<example> 
-        ///new Condition("p1.Health > p2.Health")
-        ///</example>
-        ///<exception cref="ArgumentNullException">Thrown when the condition is null</exception>
-        ///<returns>A new condition</returns>
-        [JsonConstructor]
-        public Condition(string conditionString)
-        {
-            if (conditionString == null) throw new ArgumentNullException();
+        return Condition.Evaluate<T>(this.ConditionString, p1, p2, imports);
+    }
 
-            var options = ScriptOptions.Default.AddReferences(typeof(T).Assembly).AddImports("Player");
+    ///<summary>
+    ///Activates the effect
+    ///</summary>
+    ///<param name="p1">The first parameter</param>
+    ///<param name="p2">The second parameter</param>
+    ///<exception cref="ArgumentNullException">Thrown when the parameters are null</exception>
+    public void Activate<T>(T p1, T p2, params string[] imports)
+    {
+        if (p1 == null) throw new ArgumentNullException(nameof(p1));
+        if (p2 == null) throw new ArgumentNullException(nameof(p2));
 
-            var t = CSharpScript.EvaluateAsync<Func<T, T, bool>>(conditionString, options);
+        Action.Execute<T>(this.ActionString, p1, p2, imports);
+    }
 
-            t.Wait();
+}
 
-            this.ConditionString = conditionString;
+///<summary>
+///Represents an contion for an `Effect`
+///</summary>
+public static class Condition
+{
+    ///<summary>
+    ///Checks if the condition is correct
+    ///</summary>
+    ///<param name="conditionString">The condition as a string</param>
+    ///<param name="imports">The imports for the condition used by `CSharpScript`</param>
+    ///<exception cref="ArgumentNullException">Thrown when the condition is null</exception>
+    ///<exception cref="CompilationErrorException">Thrown when the condition is not correct</exception>
+    ///<returns>True if the condition is correct</returns>
+    public static bool CheckIsCorrect<T>(string conditionString, params string[] imports)
+    {
+        if (conditionString == null) throw new ArgumentNullException(nameof(conditionString));
 
-            this._condition = t.Result;
-        }
+        var options = ScriptOptions.Default.AddReferences(typeof(T).Assembly).AddImports(imports);
 
+        var script = CSharpScript.Create<Func<T, T, bool>>(conditionString, options);
+
+        var compileResult = script.Compile();
+
+        if (compileResult.Length > 0)
+            throw new CompilationErrorException(compileResult[0].GetMessage(), compileResult);
+
+        return true;
     }
     ///<summary>
-    ///Represents an action for an `Effect`
+    ///Evaluates the condition
     ///</summary>
-    public class Action<T>
+    ///<param name="conditionString">The condition as a string</param>
+    ///<param name="p1">The first parameter</param>
+    ///<param name="p2">The second parameter</param>
+    ///<param name="imports">The imports for the condition used by `CSharpScript`</param>
+    ///<exception cref="ArgumentNullException">Thrown when the condition is null</exception>
+    ///<exception cref="CompilationErrorException">Thrown when the condition is not correct</exception>
+    public static bool Evaluate<T>(string conditionString, T p1, T p2, params string[] imports)
     {
-        ///<summary>
-        ///The action to execute
-        ///</summary>
-        [JsonIgnore]
-        public Action<T, T> _action { get; init; }
+        if (p1 == null) throw new ArgumentNullException(nameof(p1));
+        if (p2 == null) throw new ArgumentNullException(nameof(p2));
 
-        ///<summary>
-        ///The action as a string
-        ///</summary>
-        public string ActionString { get; private init; }
+        var options = ScriptOptions.Default.AddReferences(typeof(T).Assembly).AddImports(imports);
+
+        var scriptRunner = CSharpScript.EvaluateAsync<Func<T, T, bool>>(conditionString, options);
+
+        scriptRunner.Wait();
+
+        return scriptRunner.Result.Invoke(p1, p2);
+
+    }
 
 
-        ///<summary>
-        ///Executes the action
-        ///</summary>
-        ///<param name="p1">The first player</param>
-        ///<param name="p2">The second player</param>
-        ///<exception cref="ArgumentNullException">Thrown when the first or second player is null</exception>
-        public void Execute(T p1, T p2)
-        {
-            if (p1 == null || p2 == null) throw new ArgumentNullException();
-            _action.Invoke(p1, p2);
-        }
+}
+///<summary>
+///Represents an action for an `Effect`
+///</summary>
+public static class Action
+{
+    ///<summary>
+    ///Executes the action
+    ///</summary>
+    ///<param name="actionString">The action as a string</param>
+    ///<param name="p1">The first parameter</param>
+    ///<param name="p2">The second parameter</param>
+    ///<param name="imports">The imports for the action used by `CSharpScript`</param>
+    ///<exception cref="ArgumentNullException">Thrown when the action is null</exception>
+    ///<exception cref="CompilationErrorException">Thrown when the action is not correct</exception>
+    public static void Execute<T>(string actionString, T p1, T p2, params string[] imports)
+    {
+        if (p1 == null) throw new ArgumentNullException(nameof(p1));
+        if (p2 == null) throw new ArgumentNullException(nameof(p2));
 
-        ///<summary>
-        ///Creates a new action
-        ///</summary>
-        ///<param name="actionString">The action to execute</param>
-        ///<example>
-        ///new Action("p1.Health -= 10")
-        ///</example>
-        ///<exception cref="ArgumentNullException">Thrown when the action is null</exception>
-        ///<returns>A new action</returns>
-        [JsonConstructor]
-        public Action(string actionString)
-        {
-            if (actionString == null) throw new ArgumentNullException();
+        var options = ScriptOptions.Default.AddReferences(typeof(T).Assembly).AddImports(imports);
 
-            var options = ScriptOptions.Default.AddReferences(typeof(T).Assembly).AddImports("Player");
+        var scriptRunner = CSharpScript.EvaluateAsync<Action<T, T>>(actionString, options);
 
-            var t = CSharpScript.EvaluateAsync<Action<T, T>>(actionString, options);
+        scriptRunner.Wait();
 
-            t.Wait();
+        scriptRunner.Result.Invoke(p1, p2);
+    }
 
-            this.ActionString = actionString;
+    ///<summary>
+    ///Checks if the action is correct
+    ///</summary>
+    ///<param name="actionString">The action as a string</param>
+    ///<param name="import">The imports for the action used by `CSharpScript`</param>
+    ///<exception cref="ArgumentNullException">Thrown when the action is null</exception>
+    ///<exception cref="CompilationErrorException">Thrown when the action is not correct</exception>
+    ///<returns>True if the action is correct</returns>
+    public static bool CheckIsCorrect<T>(string actionString, params string[] import)
+    {
+        if (actionString == null) throw new ArgumentNullException(nameof(actionString));
 
-            this._action = t.Result;
-        }
+        var options = ScriptOptions.Default.AddReferences(typeof(T).Assembly).AddImports(import);
+
+        var script = CSharpScript.Create<Action<T, T>>(actionString, options);
+
+        var compileResult = script.Compile();
+
+        if (compileResult.Length > 0)
+            throw new CompilationErrorException(compileResult[0].GetMessage(), compileResult);
+
+        return true;
     }
 }
