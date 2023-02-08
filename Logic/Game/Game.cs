f269faca9193c296c757abf5825999e4ed89bfe1
Rafael Acosta <theGitNoob@gmail.com>
2023-02-08 12:07:23 -0500

    Added Change Defaults Method

--- Logic/Game/Game.cs
+++ Logic/Game/Game.cs
@@ -7,47 +7,77 @@ using System.Text.Json;
 
 namespace Game;
 
-///<summary>
-///The brinde between UI and Logic
-///</summary>
+/// <summary>
+/// The bridge between UI and Logic
+/// </summary>
 public static class GameController
 {
-    ///<summary>
-    ///The cards in the game
-    ///</summary>
+    /// <summary>
+    /// The cards in the game
+    /// </summary>
     public static List<SimpleCard> Cards { get; private set; } = new List<SimpleCard>();
 
-    ///<summary>
-    ///The path to the items.json file
-    ///</summary>
+    /// <summary>
+    /// The path to the items.json file
+    /// </summary>
     public static string itemsFilePath = "../Content/items.json";
 
-    ///<summary>
-    ///The path to the heros.json file
-    ///</summary>
+    /// <summary>
+    /// The path to the heros.json file
+    /// </summary>
     public static string heroesFilePath = "../Content/heroes.json";
 
-    ///<summary>
-    ///The path to the cards directory
-    ///</summary>
+    /// <summary>
+    /// The path to the cards directory
+    /// </summary>
     public static string cardsDir = "../Content/";
 
+    /// <summary>
+    /// The Initial HP points of each player
+    /// </summary>
+    static int _hpPoints { get; set; } = 4000;
 
-    ///<summary>
-    ///Starts the game
-    ///</summary>
+    /// <summary>
+    /// The Initial Cards of each player
+    /// </summary>
+    static int _initialCards { get; set; } = 5;
+
+    /// <summary>
+    /// The number of cards each player can draw per turn
+    /// </summary>
+    static int _cardsPerTurn { get; set; } = 1;
+
+    /// <summary>
+    /// The minimum number of cards a valid Deck should have
+    /// </summary>
+    static int _minDeckCards { get; set; } = 1;
+
+    /// <summary>
+    /// The maximum number of cards a valid Deck should have
+    /// </summary>
+    static int _maxDeckCards { get; set; } = 1;
+
+    /// <summary>
+    /// The number of players in the game
+    /// </summary>
+    static int _playersCnt { get; set; } = 2;
+
+
+    /// <summary>
+    /// Starts the game
+    /// </summary>
     public static void StartGame()
     {
         LoadCards();
     }
 
 
-    ///<summary>
-    ///Creates a new `Effect` if is correct
-    ///</summary>
-    ///<param name="condition">The condition for the effect</param>
-    ///<param name="action">The action for the effect</param>
-    ///<returns>A new `Effect`</returns>
+    /// <summary>
+    /// Creates a new `Effect` if is correct
+    /// </summary>
+    /// <param name="condition">The condition for the effect</param>
+    /// <param name="action">The action for the effect</param>
+    /// <returns>A new `Effect`</returns>
     public static Effect CreateEffect(string condition, string action)
     {
         Effect.CheckIsCorrect<SimplePlayer>(condition, action, "Player");
@@ -57,18 +87,18 @@ public static class GameController
         return effect;
     }
 
-    ///<summary>
-    ///Creates a new `HeroCard`
-    ///</summary>
-    ///<param name="name">The name of the `HeroCard`</param>
-    ///<param name="attack">The attack of the `HeroCard`</param>
-    ///<param name="defense">The defense of the `HeroCard`</param>
-    ///<param name="description">The description of the card</param>
-    ///<param name="condition">The condition for the effect</param>
-    ///<param name="action">The action for the effect</param>
-    ///<exception cref="ArgumentNullException">Thrown when the name, description, condition or action is null</exception>
-    ///<exception cref="Exception">Thrown when there is already another card with the same name</exception>
-    ///<exception cref="CompilationErrorException">Thrown when the condition or action are not correct</exception>
+    /// <summary>
+    /// Creates a new `HeroCard`
+    /// </summary>
+    /// <param name="name">The name of the `HeroCard`</param>
+    /// <param name="attack">The attack of the `HeroCard`</param>
+    /// <param name="defense">The defense of the `HeroCard`</param>
+    /// <param name="description">The description of the card</param>
+    /// <param name="condition">The condition for the effect</param>
+    /// <param name="action">The action for the effect</param>
+    /// <exception cref="ArgumentNullException">Thrown when the name, description, condition or action is null</exception>
+    /// <exception cref="Exception">Thrown when there is already another card with the same name</exception>
+    /// <exception cref="CompilationErrorException">Thrown when the condition or action are not correct</exception>
     public static void CreateHeroCard(string? name, int attack, int defense, string? description, string? condition, string? action)
     {
         if (name == null) throw new ArgumentNullException(nameof(name));
@@ -94,27 +124,27 @@ public static class GameController
     }
 
 
-    ///<summary>
-    ///Checks if there exists another card with the same name
-    ///</summary>
-    ///<param name="newCard">The card to check if already exists</param>
-    ///<returns>True if the card doensn't currently exists, false otherwise</returns>
+    /// <summary>
+    /// Checks if there exists another card with the same name
+    /// </summary>
+    /// <param name="newCard">The card to check if already exists</param>
+    /// <returns>True if the card doensn't currently exists, false otherwise</returns>
     static bool IsUnique(SimpleCard newCard)
     {
         return !Cards.Exists(card => card.Name == newCard.Name);
 
     }
 
-    ///<summary>
-    ///Creates a new `ItemCard`
-    ///</summary>
-    ///<param name="name">The name of the `ItemCard`</param>
-    ///<param name="description">The description of the card</param>
-    ///<param name="condition">The condition for the effect</param>
-    ///<param name="action">The action for the effect</param>
-    ///<exception cref="ArgumentNullException">Thrown when the name, description, condition or action is null</exception>
-    ///<exception cref="Exception">Thrown when there is already another card with the same name</exception>
-    ///<exception cref="CompilationErrorException">Thrown when the condition or action are not correct</exception>
+    /// <summary>
+    /// Creates a new `ItemCard`
+    /// </summary>
+    /// <param name="name">The name of the `ItemCard`</param>
+    /// <param name="description">The description of the card</param>
+    /// <param name="condition">The condition for the effect</param>
+    /// <param name="action">The action for the effect</param>
+    /// <exception cref="ArgumentNullException">Thrown when the name, description, condition or action is null</exception>
+    /// <exception cref="Exception">Thrown when there is already another card with the same name</exception>
+    /// <exception cref="CompilationErrorException">Thrown when the condition or action are not correct</exception>
     public static void CreateItemCard(string? name, string? description, string? condition, string? action)
     {
         if (name == null) throw new ArgumentNullException(nameof(name));
@@ -141,22 +171,22 @@ public static class GameController
     }
 
 
-    ///<summary>
-    ///Deserializes a `List<SimpleCards>` from a string formated as json
-    ///</summary>
-    ///<param name = "jsonFile"> The string to deserialize</param>
-    ///<returns>A string formated as json</returns>
+    /// <summary>
+    /// Deserializes a `List<SimpleCards>` from a string formated as json
+    /// </summary>
+    /// <param name = "jsonFile"> The string to deserialize</param>
+    /// <returns>A string formated as json</returns>
     public static List<ItemCard> DeserializeItem(string jsonFile)
     {
         List<ItemCard> items = JsonSerializer.Deserialize<List<ItemCard>>(jsonFile)!;
         return items;
     }
 
-    ///<summary>
-    ///Deserializes a `List<SimpleCards>` from a string formated as json
-    ///</summary>
-    ///<param name = "jsonFile"> The string to deserialize</param>
-    ///<returns>A string formated as json</returns>
+    /// <summary>
+    /// Deserializes a `List<SimpleCards>` from a string formated as json
+    /// </summary>
+    /// <param name = "jsonFile"> The string to deserialize</param>
+    /// <returns>A string formated as json</returns>
     public static List<HeroCard> DeserializeHeros(string jsonFile)
     {
         List<HeroCard> heroes = JsonSerializer.Deserialize<List<HeroCard>>(jsonFile)!;
@@ -164,10 +194,10 @@ public static class GameController
     }
 
 
-    ///<summary>
-    ///Serializes a `List<ItemCard>` into a string formated as json
-    ///</summary>
-    ///<returns>A string formated as json</returns>
+    /// <summary>
+    /// Serializes a `List<ItemCard>` into a string formated as json
+    /// </summary>
+    /// <returns>A string formated as json</returns>
     public static string SerializeHeroes()
     {
         List<HeroCard> heroes = Cards.OfType<HeroCard>().ToList();
@@ -179,10 +209,10 @@ public static class GameController
         return jsonFile;
     }
 
-    ///<summary>
-    ///Serializes a `List<ItemCard>` into a string formated as json
-    ///</summary>
-    ///<returns>A string formated as json</returns>
+    /// <summary>
+    /// Serializes a `List<ItemCard>` into a string formated as json
+    /// </summary>
+    /// <returns>A string formated as json</returns>
     public static string SerializeItems()
     {
         List<ItemCard> items = Cards.OfType<ItemCard>().ToList();
@@ -195,9 +225,9 @@ public static class GameController
     }
 
 
-    ///<summary>
-    ///Loads cards from disk on json format
-    ///</summary>
+    /// <summary>
+    /// Loads cards from disk on json format
+    /// </summary>
     public static void LoadCards()
     {
         if (!Directory.Exists(cardsDir))
@@ -233,9 +263,9 @@ public static class GameController
 
     }
 
-    ///<summary>
-    ///Saves cards to disk on json format
-    ///</summary>
+    /// <summary>
+    /// Saves cards to disk on json format
+    /// </summary>
     public static void SaveCards()
     {
         string itemsJsonFile = SerializeItems();
@@ -248,30 +278,40 @@ public static class GameController
     }
 
 
-    ///<summary>
-    ///Creates a new game
-    ///</summary>
-    ///<param name="maxHeroCards">The maximum number of hero cards a player can have</param>
-    ///<param name="maxItemCards">The maximum number of item cards a player can have</param>
-    ///<param name="minDeckCards">The minimum number of cards a player can have in their deck</param>
-    ///<param name="maxDeckCards">The maximum number of cards a player can have in their deck</param>
-    public static void NewGame(int maxHeroCards = 5, int maxItemCards = 5, int minDeckCards = 1, int maxDeckCards = 50)
+    /// <summary>
+    /// Creates a new game
+    /// </summary>
+    public static void NewGame(List<SimplePlayer> players)
     {
+        if (players.Count != _playersCnt) throw new ArgumentException("The number of players is not correct", nameof(players));
 
-        SimpleDeck d1 = new SimpleDeck(Cards, minDeckCards, maxDeckCards);
-        SimpleDeck d2 = new SimpleDeck(Cards, minDeckCards, maxDeckCards);
-
-        AIPlayer p1 = new AIPlayer("Player 1", 4000, maxHeroCards, maxItemCards, d1);
-        AIPlayer p2 = new AIPlayer("Player 2", 4000, maxHeroCards, maxItemCards, d2);
-
-        GameLoop.GameLoop loop = new GameLoop.GameLoop(new List<SimplePlayer> { p1, p2 });
+        Game.GameLoop loop = new Game.GameLoop(players);
 
         loop.StartGame();
     }
 
-    ///<summary>
-    ///Saves all the cards to disk and gracefully shutdown
-    ///</summary>
+    public static void ChangeDefaults(int hpPoints, int initialCards, int cardsPerTurn, int minDeckCards, int maxDeckCards, int playersCnt)
+    {
+        if (hpPoints < 1) throw new ArgumentOutOfRangeException(nameof(hpPoints), "The hp points must be greater than 0");
+        if (initialCards < 1) throw new ArgumentOutOfRangeException(nameof(initialCards), "The initial cards must be greater than 0");
+        if (cardsPerTurn < 1) throw new ArgumentOutOfRangeException(nameof(cardsPerTurn), "The cards per turn must be greater than 0");
+        if (minDeckCards < 1) throw new ArgumentOutOfRangeException(nameof(minDeckCards), "The minimum deck cards must be greater than 0");
+        if (maxDeckCards < 1) throw new ArgumentOutOfRangeException(nameof(maxDeckCards), "The maximum deck cards must be greater than 0");
+        if (minDeckCards > maxDeckCards) throw new ArgumentException($"{nameof(minDeckCards)} should be less or equal than {nameof(maxDeckCards)}");
+        if (playersCnt < 2) throw new ArgumentOutOfRangeException(nameof(playersCnt), "The players count must be greater than 1");
+
+        _hpPoints = hpPoints;
+        _initialCards = initialCards;
+        _cardsPerTurn = cardsPerTurn;
+        _minDeckCards = minDeckCards;
+        _maxDeckCards = maxDeckCards;
+        _playersCnt = playersCnt;
+
+    }
+
+    /// <summary>
+    /// Saves all the cards to disk and gracefully shutdown
+    /// </summary>
     public static void ExitGame()
     {
         Environment.Exit(0);
