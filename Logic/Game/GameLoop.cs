1b66f048d811b11456a6ad8ef14258cd1686cbf0
Rafael Acosta <theGitNoob@gmail.com>
2023-02-14 10:50:57 -0500

    Reimplemented the Loop Logic

--- Logic/Game/GameLoop.cs
+++ Logic/Game/GameLoop.cs
@@ -5,110 +5,81 @@ namespace Game;
 /// <summary>
 /// The game loop
 /// </summary>
-public class GameLoop
+public static class GameLoop
 {
-    /// <summary>
-    /// The players in the game
-    /// </summary>
-    List<SimplePlayer> _players { get; set; }
 
-    /// <summary>
-    /// Creates a new `GameLoop`
-    /// </summary>
-    /// <param name="players">The players in the game</param>
-    public GameLoop(List<SimplePlayer> players)
-    {
-        this._players = players;
-
-        for (int curr = 0; curr < players.Count; curr++)
-        {
-            for (int next = curr + 1; next < players.Count; next++)
-            {
-                AIPlayer p1 = (AIPlayer)players[curr];
-                AIPlayer p2 = (AIPlayer)players[next];
-
-                p1.SetEnemy(p2);
-                p2.SetEnemy(p1);
-
-            }
-        }
-    }
 
     /// <summary>
     /// Starts the game
     /// </summary>
-    public void StartGame()
+    public static void StartGame(int initialCards, int cardsPerTurn, SimplePlayer p1, SimplePlayer p2)
     {
         bool canAttack = false;
 
-        foreach (SimplePlayer player in _players)
-        {
-            player.Deck.Shuffle();
-        }
+        p1.SetEnemy(p2);
+        p2.SetEnemy(p1);
 
-        int cardsToDraw = 4;
+        // System.Console.WriteLine(p1.GetType());
+        // System.Console.WriteLine(p2.GetType());
+
+        p1.Deck.Shuffle();
+        p2.Deck.Shuffle();
+
+        int cardsToDraw = initialCards;
 
         while (true)
         {
-            Printer.Print(_players[0], _players[1]);
+            Printer.Print(p1, p2);
 
-            for (int idx = 0; idx < _players.Count; idx++)
+            if (CheckPlayerHasLost(p1))
             {
-                var player = _players[idx];
-
-                //The last player is the first to attack
-                if (idx == _players.Count - 1) canAttack = true;
-
-                if (CheckPlayerHasLost(player))
-                {
-                    EndGame(player);
-                    return;
-                }
-
-                if (player is AIPlayer)
-                {
-                    AIPlayer aiPlayer = (AIPlayer)player;
-                    aiPlayer.DrawCards(cardsToDraw);
-                    aiPlayer.Play(canAttack);
-                }
-
-                Task.Delay(500).Wait();
-                Printer.Print(_players[0], _players[1]);
-
+                EndGame(p1, p2);
+                return;
             }
-            cardsToDraw = 1;
-        }
 
+            p1.DrawCards(cardsToDraw);
+            p1.Play(canAttack);
+
+            Task.Delay(500).Wait();
+
+            Printer.Print(p1, p2);
+
+            canAttack = true;
+
+            if (CheckPlayerHasLost(p2))
+            {
+                EndGame(p2, p1);
+                return;
+            }
+
+            p2.DrawCards(cardsToDraw);
+            p2.Play(canAttack);
+
+            Task.Delay(500).Wait();
+
+            cardsToDraw = cardsPerTurn;
+        }
     }
 
     /// <summary>
     /// Ends the game and prints the winning player
     /// </summary>
-    public void EndGame(SimplePlayer looserPlayer)
+    static void EndGame(SimplePlayer looser, SimplePlayer winner)
     {
-        foreach (SimplePlayer player in _players)
-        {
-            if (player == looserPlayer)
-            {
-                System.Console.WriteLine($"{player.Name} has lost");
-            }
-            else
-            {
-                System.Console.WriteLine($"{player.Name} has won");
-            }
-        }
-
-        _players = new List<SimplePlayer>();
+        Console.WriteLine($"{looser.Name} has lost");
+        Console.WriteLine($"{winner.Name} has won");
     }
+
+
 
     /// <summary>
     /// Checks whether a player has lost because of his HP or his hand
     /// </summary>
     /// <param name="player">The player to check</param>
     /// <returns>True if the player has lost, false otherwise</returns>
-    public bool CheckPlayerHasLost(SimplePlayer player)
+    static bool CheckPlayerHasLost(SimplePlayer player)
     {
-        if (player.HP == 0 || player.Deck.IsEmpty())
+        if (player.Hp == 0 || player.Deck.IsEmpty())
             return true;
 
         return false;
